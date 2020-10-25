77a50521793b856241dbdbfb72c296ce38c2323d
imsy875 <64747130+imsy875@users.noreply.github.com>
2020-10-26 01:05:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,55 +1,27 @@
-from flask import Flask, render_template, request
-import jsonify
-import requests
-import pickle
 import numpy as np
-import sklearn
-from sklearn.preprocessing import StandardScaler
+from flask import Flask, request, jsonify, render_template
+import pickle
+
 app = Flask(__name__)
-model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
-@app.route('/',methods=['GET'])
-def Home():
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
     return render_template('index.html')
 
-
-standard_to = StandardScaler()
-@app.route("/predict", methods=['POST'])
+@app.route('/predict',methods=['POST'])
 def predict():
-    Fuel_Type_Diesel=0
-    if request.method == 'POST':
-        Year = int(request.form['Year'])
-        Present_Price=float(request.form['Present_Price'])
-        Kms_Driven=int(request.form['Kms_Driven'])
-        Kms_Driven2=np.log(Kms_Driven)
-        Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
-        if(Fuel_Type_Petrol=='Petrol'):
-                Fuel_Type_Petrol=1
-                Fuel_Type_Diesel=0
-        elif(Fuel_Type_Petrol=='Diesel'):
-                Fuel_Type_Petrol=0
-                Fuel_Type_Diesel=1
-        else:
-            Fuel_Type_Petrol=0
-            Fuel_Type_Diesel=0
-        Year=2020-Year
-        Seller_Type_Individual=request.form['Seller_Type_Individual']
-        if(Seller_Type_Individual=='Individual'):
-            Seller_Type_Individual=1
-        else:
-            Seller_Type_Individual=0	
-        Transmission_Mannual=request.form['Transmission_Mannual']
-        if(Transmission_Mannual=='Mannual'):
-            Transmission_Mannual=1
-        else:
-            Transmission_Mannual=0
-        prediction=model.predict([[Present_Price,Kms_Driven2,Year,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
-        output=round(prediction[0],4)
-        if output<0:
-            return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
-        else:
-            return render_template('index.html',prediction_text="You Can Sell The Car at {} Lakhs".format(output))
-    else:
-        return render_template('index.html')
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
 
-    if __name__ == '__main__':
-    app.run()
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
